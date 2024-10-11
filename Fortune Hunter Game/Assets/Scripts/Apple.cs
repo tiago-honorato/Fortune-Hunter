8b90a540bd89e67dd71a3aee15ca7115c309e001
Tiago <tiagohirute@gmail.com>
2024-10-11 02:03:11 -0300

    itens pick added

---
+++ Fortune Hunter Game/Assets/Scripts/Apple.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public GameObject collected;
+    public int Score;
+
+
+    void Start()
+    {
+        
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        
+        if (collider.gameObject.tag == "Player")
+        {
+            
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            GameController.instance.totalScore += Score;
+
+            Destroy(gameObject, 0.25f);
+
+        }
+
+    }
+
+}
