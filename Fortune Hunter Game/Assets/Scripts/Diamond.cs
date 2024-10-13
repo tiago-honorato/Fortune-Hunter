7ef12840a8fdaca24149cdac5fcd78242acc2e1b
Tiago <tiagohirute@gmail.com>
2024-10-12 23:20:39 -0300

    sprite diamond fixed

--- Fortune Hunter Game/Assets/Scripts/Diamond.cs
+++ Fortune Hunter Game/Assets/Scripts/Diamond.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour
+public class Diamond : MonoBehaviour
 {
 
     private SpriteRenderer sr;
