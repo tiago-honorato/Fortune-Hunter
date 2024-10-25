2066f5a1ad6fd73034d49faef090f821a09346ec
Tiago <tiagohirute@gmail.com>
2024-10-25 16:12:08 -0300

    temp att

--- Fortune Hunter Game/Assets/Scripts/Goblin.cs
+++ Fortune Hunter Game/Assets/Scripts/Goblin.cs
@@ -47,13 +47,14 @@ public class Goblin : MonoBehaviour
 
     }
 
+    bool playerDestroyed = false;
     void OnCollisionEnter2D(Collision2D col){
 
         if (col.gameObject.tag == "Player")
         {
             float height = col.contacts[0].point.y - headPoint.position.y;
 
-            if (height > 0)
+            if (height > 0 && !playerDestroyed)
             {
                 col.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 10, ForceMode2D.Impulse);
                 speed = 0;
@@ -62,6 +63,12 @@ public class Goblin : MonoBehaviour
                 circleCollider2D.enabled = false;
                 rig.bodyType = RigidbodyType2D.Kinematic;
                 Destroy(gameObject, 0.33f);
+            }else{
+
+                playerDestroyed = true;
+                GameController.instance.ShowGameOver();
+                Destroy(col.gameObject);
+
             }
 
         }
