3c60479496b87d0289c43fe7e8ec47b9c889ec02
Tiago <tiagohirute@gmail.com>
2024-10-27 03:58:05 -0300

    player move fixed

--- Fortune Hunter Game/Assets/Scripts/Player.cs
+++ Fortune Hunter Game/Assets/Scripts/Player.cs
@@ -30,21 +30,21 @@ public class Player : MonoBehaviour
 
     void Move(){
 
-        
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
+        float movement = Input.GetAxis("Horizontal");
 
-        if (Input.GetAxis("Horizontal") > 0f)
+        rig.velocity = new Vector2(movement * Speed, rig.velocity.y);
+
+        if (movement > 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 0f , 0f);
         }
-        if (Input.GetAxis("Horizontal") < 0f)
+        if (movement < 0f)
         {
             anim.SetBool("walk", true);
             transform.eulerAngles = new Vector3(0f, 180f , 0f);
         }
-        if (Input.GetAxis("Horizontal") == 0f)
+        if (movement == 0f)
         {
             anim.SetBool("walk", false);
         }
