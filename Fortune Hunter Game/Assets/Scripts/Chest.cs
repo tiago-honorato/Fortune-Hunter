ea79194b16ae7a663d23d8ab1d1aefb7ce5d253c
Tiago <tiagohirute@gmail.com>
2024-11-21 15:02:03 -0300

    heart fixed

--- Fortune Hunter Game/Assets/Scripts/Chest.cs
+++ Fortune Hunter Game/Assets/Scripts/Chest.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Chest : MonoBehaviour
 {
     public BoxCollider2D coll;
-
     public Animator anim;
-
     public ParticleSystem chestParticle;
+    public ParticleSystem upParticle;
     public ParticleSystem itemParticle;
+    public SpriteRenderer itemRender;
+    public CircleCollider2D itemCollider;
 
     private void Start() {
         
@@ -26,6 +27,9 @@ public class Chest : MonoBehaviour
             coll.enabled = false;
             anim.SetTrigger("open");
             chestParticle.Stop();
+            itemCollider.enabled = true;
+            itemRender.enabled = true;
+            upParticle.Play();
             itemParticle.Play();
 
             AudioController.instance.PlayCollectGemSoundEffect();
---
+++ Fortune Hunter Game/Assets/Scripts/Heart.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public ParticleSystem upParticle;
+    public ParticleSystem itemParticle;
+
+
+    public GameObject collected;
+    public int Score;
+
+
+    void Start()
+    {
+        
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        
+        if (collider.gameObject.tag == "Player")
+        {
+            
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            upParticle.Stop();
+            itemParticle.Stop();
+
+
+            AudioController.instance.PlayCollectGemSoundEffect();
+
+            GameController.instance.totalScore += Score;
+            GameController.instance.UpdateScoreText();
+
+            Destroy(gameObject, 0.25f);
+
+        }
+
+    }
+
+}
