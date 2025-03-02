f5f655b89e9921d78a2a8a64aa49bf0e8c518125
Tiago <tiagohirute@gmail.com>
2025-03-02 01:41:58 -0300

    minor changes

---
+++ Fortune Hunter Game/Assets/Scripts/Heart_Item.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Heart_Item : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    private CircleCollider2D circle;
+
+    public ParticleSystem upParticle;
+    public ParticleSystem itemParticle;
+
+
+    public GameObject collected;
+    public int Score;
+
+
+    void Start()
+    {
+        
+        sr = GetComponent<SpriteRenderer>();
+        circle = GetComponent<CircleCollider2D>();
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        
+        if (collider.gameObject.tag == "Player")
+        {
+            
+            sr.enabled = false;
+            circle.enabled = false;
+            collected.SetActive(true);
+
+            AudioController.instance.PlayCollectGemSoundEffect();
+
+            ScoreManager.instance.AddLife(1);
+            GameController.instance.UpdateLifeText();
+
+            Destroy(gameObject, 0.25f);
+
+        }
+
+    }
+
+}
