05414d4a21ad82756174c8fecf06498191890ad0
Tiago <tiagohirute@gmail.com>
2025-04-13 21:43:01 -0300

    level 18 finished

--- Fortune Hunter Game/Assets/Scripts/BoxKiller.cs
+++ Fortune Hunter Game/Assets/Scripts/BoxKiller.cs
@@ -4,7 +4,7 @@ public class BoxKiller : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("box"))
+        if (other.CompareTag("box") || other.CompareTag("Saw"))
         {
             Destroy(other.gameObject); // Destrói a caixa
         }
